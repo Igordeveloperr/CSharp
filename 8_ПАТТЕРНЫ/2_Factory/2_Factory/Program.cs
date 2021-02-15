5e21bb0b2e40411855c723101aa203f3d82e95b4
Igordeveloperr <g.bikmetow@gmail.com>
2021-02-15 23:42:21 +0300

    дописать пример factory паттерна

---
+++ 8_ПАТТЕРНЫ/2_Factory/2_Factory/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var massenger = new Messanger("Igor", "1234");
+            massenger.SendMessage("hello C#", "localhost", "first");
+        }
+    }
+}
---
+++ 8_ПАТТЕРНЫ/2_Factory/2_Factory/factory_pattern/TwitterMessanger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Factory.factory_pattern
+{
+    class TwitterMessanger : MessageBase
+    {
+        public TwitterMessanger(string txt, string source, string target) : base(txt, source, target) {}
+        public override void Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
