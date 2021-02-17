ac9b307bd9a8feff0f0907e9267e7671bd902f5e
Igordeveloperr <g.bikmetow@gmail.com>
2021-02-18 00:23:37 +0300

    я ебанулся головооой по wpf

---
+++ 9_WPF/2_Файлы_отдельного_кода/2_Файлы_отдельного_кода/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_Файлы_отдельного_кода
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // событие, которое я повесил на кнопку в верстке xaml
+        private void Button_Down(object sender, RoutedEventArgs e)
+        {
+            string txt = input.Text;
+            if (!string.IsNullOrWhiteSpace(txt))
+            {
+                MessageBox.Show(txt);
+                AddButton(txt);
+            }
+        }
+        // создание UI елемента через код C#
+        private void AddButton(string btnText)
+        {
+            Button button = new Button();
+            button.Width = 200;
+            button.Height = 200;
+            button.Content = btnText;
+            button.VerticalAlignment = VerticalAlignment.Bottom;
+            button.HorizontalAlignment = HorizontalAlignment.Left;
+            gridLayout.Children.Add(button);
+        }
+    }
+}
