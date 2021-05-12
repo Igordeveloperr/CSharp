2311bbd1274f98471dd9d6ac047d7aaa7b32cf28
Igordeveloperr <g.bikmetow@gmail.com>
2021-05-12 22:37:32 +0400

    ОГО ОГО ОГО типа тупа создание своих компонентов обработки запроса

---
+++ 11_ASP/6_Определение_компонентов/6_Определение_компонентов/TokenExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_Определение_компонентов
+{
+    public static class TokenExtensions
+    {
+        // метод расширения
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder, string pattern)
+        {
+            return builder.UseMiddleware<TokenMiddleware>(pattern);
+        }
+    }
+}
