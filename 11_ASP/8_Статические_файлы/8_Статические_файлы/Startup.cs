2e54d4b4d1ffae4bbaf23ea626536ae24a9247a4
Igordeveloperr <g.bikmetow@gmail.com>
2021-05-21 00:35:09 +0400

    стоп сто статические файлы

---
+++ 11_ASP/8_Статические_файлы/8_Статические_файлы/Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace _8_Статические_файлы
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //UseDefaultFiles() можно настроить отправку статических веб-страниц по умолчанию без обращения к ним по полному пути
+            app.UseDefaultFiles();
+            // разрешаю использованрие статических файлов
+            app.UseStaticFiles();
+            // возможность обратиться к файлу через запрос
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                RequestPath = new PathString("/pages")
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("shooo");
+            });
+        }
+    }
+}
