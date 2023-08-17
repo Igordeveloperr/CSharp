e552e03a60840a9eff15fb422a8ed74b5a65f962
Igordeveloperr <g.bikmetow@gmail.com>
2023-08-17 11:16:33 +0300

    адски трахнул потоки с файлами

---
+++ 12_закрываем_пробелы/3_Потоки_и_файлы/3_Потоки_и_файлы/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+string path = "./data.txt";
+// using - создает локальную область видимости и удаляет объект через Dispose после выхода из нее
+using(var writer = new StreamWriter(path, true, Encoding.UTF8))
+{
+    // записываем данные в файл
+    writer.WriteLine("Я ХОЧУ ПИЦЦЫ");
+}
+
+using(var reader = new StreamReader(path))
+{
+    // читаем данные из файла
+    Console.WriteLine(reader.ReadToEnd());
+}
