9a499bede28ee61746af8e6083fb90a93ece0d87
Igordeveloperr <g.bikmetow@gmail.com>
2023-08-19 11:33:29 +0300

    закоментил

--- 12_закрываем_пробелы/test/test/Program.cs
+++ 12_закрываем_пробелы/test/test/Program.cs
@@ -29,11 +29,12 @@ int? x = age + 10; // результатом всегда должен быть 
 #endregion
 
 #region operation ??
-string? token = null;
+/*string? token = null;
 string accessToken = token ?? "ah11dao921kl2j1g";
 Console.WriteLine(accessToken);
 
 int? x = 299;
 int y = x ?? 1488;
-Console.WriteLine(x);
+Console.WriteLine(x);*/
 #endregion
+
