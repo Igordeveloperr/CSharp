58c704a2c6e5f716c7a1ad5c921866eaa37ccf8a
Igordeveloperr <g.bikmetow@gmail.com>
2023-08-19 10:24:07 +0300

    описал структуру

---
+++ 12_закрываем_пробелы/test/test/MyFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal struct MyFile
+    {
+        const int MAX_SIZE = 1024;
+        private byte[] _size;
+        private string _path;
+
+        public MyFile(string path)
+        {
+            _size = new byte[MAX_SIZE];
+            _path = path;
+        }
+
+        public void PrintPath()
+        {
+            Console.WriteLine(_path);
+        }
+    }
+}
