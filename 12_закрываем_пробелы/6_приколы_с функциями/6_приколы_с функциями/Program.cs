0b8067e36aa5432cd5a94cf83cf465566d3c2b43
Igordeveloperr <g.bikmetow@gmail.com>
2023-09-05 19:36:43 +0300

    приколдосы

---
+++ 12_закрываем_пробелы/6_приколы_с функциями/6_приколы_с функциями/Program.cs
@@ -0,0 +1,26 @@
+﻿// анонимные методы
+
+MsgDelegate func = delegate (string message)
+{
+    Console.WriteLine(message);
+};
+func("hello");
+
+// лямбды
+Action print = () => Console.WriteLine("ЛЯмбдыа");
+print();
+
+MathDelegate sum = (int x, int y) => x + y;
+int res = sum(10, 67);
+Console.WriteLine(res);
+
+MathDelegate div = (int x, int y) => 
+{
+    if (y == 0) throw new DivideByZeroException("Делитель равен нулю!");
+    return x / y;
+};
+res = div(10, 3);
+Console.WriteLine(res);
+
+delegate void MsgDelegate(string text);
+delegate int MathDelegate(int x, int y);
