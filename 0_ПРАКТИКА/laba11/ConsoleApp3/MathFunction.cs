40a86b29f69082b797d8ac668fd04f2a0723778b
Igordeveloperr <g.bikmetow@gmail.com>
2023-09-21 22:41:36 +0300

    оно считает

---
+++ 0_ПРАКТИКА/laba11/ConsoleApp3/MathFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class MathFunction
+    {
+        private const int MIN_VALUE = -1;
+        private const byte ROUND = 6;
+        public double Calculate(double x)
+        {
+            double arg;
+            if(x < MIN_VALUE)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("D(f) = [-1; +infinity)");
+                arg = MIN_VALUE;
+            }
+            else
+            {
+                arg = x;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            return Math.Round(x - Math.Sqrt(Math.Log(arg + 2)), ROUND);
+        }
+
+        public double CalculateFirstDerivative(double x)
+        {
+            return Math.Round(1 - 1 / (2 * Math.Log(Math.Sqrt(x + 2)) * (x + 2)), ROUND);
+        }
+    }
+}
---
+++ 0_ПРАКТИКА/laba11/ConsoleApp3/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp3;
+
+MathFunction function = new MathFunction();
+Console.WriteLine(function.CalculateFirstDerivative(1));
+
+Console.ForegroundColor = ConsoleColor.White;
