9d9aed99d6630f4b0fb6b7b28c92e98629e622c5
Igordeveloperr <g.bikmetow@gmail.com>
2023-09-22 18:42:40 +0300

    модуль функции реализован

--- 0_ПРАКТИКА/laba11/ConsoleApp3/MathFunction.cs
+++ 0_ПРАКТИКА/laba11/ConsoleApp3/MathFunction.cs
@@ -8,28 +8,61 @@ namespace ConsoleApp3
 {
     internal class MathFunction
     {
+        // мин значение функции
         private const int MIN_VALUE = -1;
+        // до куда округляем
         private const byte ROUND = 6;
+
+        // считаем значение функции
         public double Calculate(double x)
         {
-            double arg;
             if(x < MIN_VALUE)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("D(f) = [-1; +infinity)");
-                arg = MIN_VALUE;
+                PrintError("D(f) = [-1; +infinity)");
+                throw new ArgumentException("D(f) = [-1; +infinity)");
             }
             else
             {
-                arg = x;
+                return Math.Round(x - Math.Sqrt(Math.Log(x + 2)), ROUND);
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            return Math.Round(x - Math.Sqrt(Math.Log(arg + 2)), ROUND);
         }
 
+        // считаем значение 1 производной функции
         public double CalculateFirstDerivative(double x)
         {
-            return Math.Round(1 - 1 / (2 * Math.Log(Math.Sqrt(x + 2)) * (x + 2)), ROUND);
+            if ((x > -2 && x < -1) || x > -1)
+            {
+                return Math.Round(1 - 1 / (2 * Math.Log(Math.Sqrt(x + 2)) * (x + 2)), ROUND);
+            }
+            else
+            {
+                PrintError("D(f) = (-2; -1) U (-1; +infinity)");
+                throw new ArgumentException("D(f) = (-2; -1) U (-1; +infinity)");
+            }
+        }
+
+        // считаем значение 2 производной функции
+        public double CalculateSecondDerivative(double x)
+        {
+            if ((x > -2 && x < -1) || x > -1)
+            {
+                return Math.Round(
+                    (2*Math.Log(Math.Sqrt(x+2))+1) / (4*Math.Pow(Math.Log(Math.Sqrt(x+2)), 2) * Math.Pow(x+2, 2)), 
+                    ROUND);
+            }
+            else
+            {
+                PrintError("D(f) = (-2; -1) U (-1; +infinity)");
+                throw new ArgumentException("D(f) = (-2; -1) U (-1; +infinity)");
+            }
+        }
+
+        // логирование ошибок
+        private void PrintError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.Green;
         }
     }
 }
--- 0_ПРАКТИКА/laba11/ConsoleApp3/Program.cs
+++ 0_ПРАКТИКА/laba11/ConsoleApp3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ConsoleApp3;
 
+Console.ForegroundColor = ConsoleColor.Green;
+
 MathFunction function = new MathFunction();
-Console.WriteLine(function.CalculateFirstDerivative(1));
+Console.WriteLine(function.CalculateSecondDerivative(10));
 
 Console.ForegroundColor = ConsoleColor.White;
