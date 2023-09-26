c96cd74cc3da07082497fab08665138858912ddb
Igordeveloperr <g.bikmetow@gmail.com>
2023-09-26 22:30:28 +0300

    база готова

--- 0_ПРАКТИКА/32_Резня_капусты/32_Резня_капусты/Form1.cs
+++ 0_ПРАКТИКА/32_Резня_капусты/32_Резня_капусты/Form1.cs
@@ -7,15 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _32_Резня_капусты
 {
     public partial class Form1 : Form
     {
+        // полe
+        private List<PictureBox> field = new List<PictureBox>();
+        private Random rnd = new Random(Guid.NewGuid().GetHashCode());
         public Form1()
-        {
-            
+        { 
             InitializeComponent();
+            GenerateField();
+        }
+
+        private void GenerateField()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                PictureBox box = new PictureBox();
+                box.Width = 65;
+                box.Height = 50;
+                box.Name = $"cell{i}";
+                field.Add(box);
+                string path = string.Empty;
+
+                long a = rnd.Next(0, 1000000);
+                long b = rnd.Next(0, 1000000);
+                if ((a*b) % 2 == 0)
+                {
+                    path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"img\kacan.jpg");
+                }
+                else
+                {
+                    path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"img\krot.jpg");
+                }
+                box.Image = Image.FromFile(path);
+                basePanel.Controls.Add(box);
+            }
         }
     }
 }
