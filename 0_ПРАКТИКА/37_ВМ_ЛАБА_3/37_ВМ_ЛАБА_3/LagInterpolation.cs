aa87dadb19d1655aa95dda213f772f9d44753be7
Igordeveloperr <g.bikmetow@gmail.com>
2023-11-17 23:06:45 +0300

    хуита ебаная

---
+++ 0_ПРАКТИКА/37_ВМ_ЛАБА_3/37_ВМ_ЛАБА_3/LagInterpolation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_ВМ_ЛАБА_3
+{
+    internal class LagInterpolation
+    {
+        public LagInterpolation()
+        {
+            
+        }
+
+        public double Interpolate(double x)
+        {
+            return 2.73951 * L1(x) + 2.30080 * L2(x) + 1.96864 * L3(x) + 1.78776 * L4(x) + 1.59502 * L5(x) + 1.34310 * L6(x);
+        }
+
+        private double L1(double x)
+        {
+            return (x-0.41)/(-0.06) * (x-0.47)/(-0.12) * (x-0.51)/(-0.16) * (x-0.56)/(-0.21) * (x-0.64)/(-0.29);
+        }
+
+        private double L2(double x)
+        {
+            return (x - 0.35) / (0.06) * (x - 0.47) / (-0.06) * (x - 0.51) / (-0.1) * (x - 0.56) / (-0.15) * (x - 0.64) / (-0.23);
+        }
+
+        private double L3(double x)
+        {
+            return (x - 0.35) / (0.12) * (x - 0.41) / (0.06) * (x - 0.51) / (-0.04) * (x - 0.56) / (-0.09) * (x - 0.64) / (-0.17);
+        }
+        private double L4(double x)
+        {
+            return (x - 0.35) / (0.16) * (x - 0.41) / (0.1) * (x - 0.47) / (0.04) * (x - 0.56) / (-0.05) * (x - 0.64) / (-0.13);
+        }
+        private double L5(double x)
+        {
+            return (x - 0.35) / (0.21) * (x - 0.41) / (0.15) * (x - 0.47) / (0.09) * (x - 0.51) / (0.05) * (x - 0.64) / (-0.08);
+        }
+        private double L6(double x)
+        {
+            return (x - 0.35) / (0.29) * (x - 0.41) / (0.23) * (x - 0.47) / (0.17) * (x - 0.51) / (0.13) * (x - 0.56) / (0.08);
+        }
+    }
+}
---
+++ 0_ПРАКТИКА/37_ВМ_ЛАБА_3/37_ВМ_ЛАБА_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using _37_ВМ_ЛАБА_3;
+
+#region Lagrange
+/*LagInterpolation lagInterpolation = new LagInterpolation();
+double y = Math.Round(lagInterpolation.Interpolate(0.526), 7);
+Console.WriteLine($"Ответ: y(0.526) = {y}");*/
+#endregion
+
+#region Newton
+NewtonInterpolation newtonInterpolation = new NewtonInterpolation();
+Console.WriteLine("Ответ: ");
+Console.WriteLine($"y(0.1511) = {Math.Round(newtonInterpolation.CalcPolinom(0.1511),7)}");
+Console.WriteLine($"y(0.6340) = {Math.Round(newtonInterpolation.CalcPolinom(0.6340),7)}");
+Console.WriteLine($"y(0.143) = {Math.Round(newtonInterpolation.CalcPolinom(0.143), 7)}");
+Console.WriteLine($"y(0.673) = {Math.Round(newtonInterpolation.CalcPolinom(0.673), 7)}");
+#endregion
