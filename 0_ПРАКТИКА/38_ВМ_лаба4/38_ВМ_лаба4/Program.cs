12c6b40e649a7079599380ab13b2f8eb4ee4cd11
Igordeveloperr <g.bikmetow@gmail.com>
2023-12-09 15:52:21 +0300

    ну типа считает

--- 0_ПРАКТИКА/38_ВМ_лаба4/38_ВМ_лаба4/Program.cs
+++ 0_ПРАКТИКА/38_ВМ_лаба4/38_ВМ_лаба4/Program.cs
@@ -1,7 +1,12 @@
 ﻿using _38_ВМ_лаба4;
 
-Console.WriteLine("Вычислить определенный интеграл с помощью формулы Симпсона");
+#region Simpson
+/*Console.WriteLine("Вычислить определенный интеграл с помощью формулы Симпсона");
 Console.WriteLine("---------------------------------------------------------------");
 var simps = new SimpsonMethod(1,2,100);
 var res = simps.CalcSquare();
-Console.WriteLine($"Ответ: {Math.Round(res, 6)}");
+Console.WriteLine($"Ответ: {Math.Round(res, 6)}");*/
+#endregion
+
+TrapezoidMethod trap = new TrapezoidMethod(0.2, 1);
+Console.WriteLine(trap.CalcSquare());
---
+++ 0_ПРАКТИКА/38_ВМ_лаба4/38_ВМ_лаба4/TrapezoidMethod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_ВМ_лаба4
+{
+    internal class TrapezoidMethod
+    {
+        const double E = 0.0001;
+        private double start;
+        private double end;
+        private double h;
+
+        public TrapezoidMethod(double a, double b)
+        {
+            start = a;
+            end = b;
+            h = E;
+        }
+
+        public double CalcSquare()
+        {
+            double sum = 0;
+            sum += 1 / 2 * CalcFunc(start);
+            for (double i = start+h; i < end; i += h)
+            {
+                sum += CalcFunc(i);
+            }
+            sum += 1 / 2 * CalcFunc(end);
+            return sum * h;
+        }
+
+        private double CalcFunc(double x)
+        {
+            return Math.Tan(x*x) / (x*x + 1);
+        }
+
+        private double CalcStep()
+        {
+            return Math.Sqrt(E);
+        }
+    }
+}
