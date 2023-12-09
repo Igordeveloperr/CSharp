2d83978f52cc2c0e00fe0791638755ef604ff7cf
Igordeveloperr <g.bikmetow@gmail.com>
2023-12-09 15:06:43 +0300

    симпсон считает правильно

---
+++ 0_ПРАКТИКА/38_ВМ_лаба4/38_ВМ_лаба4/Program.cs
@@ -0,0 +1,7 @@
+﻿using _38_ВМ_лаба4;
+
+Console.WriteLine("Вычислить определенный интеграл с помощью формулы Симпсона");
+Console.WriteLine("---------------------------------------------------------------");
+var simps = new SimpsonMethod(1,2,100);
+var res = simps.CalcSquare();
+Console.WriteLine($"Ответ: {Math.Round(res, 6)}");
