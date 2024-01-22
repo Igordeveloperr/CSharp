bd7efee3ea80a4f9b04442b38c4d4e465f20885e
Igordeveloperr <g.bikmetow@gmail.com>
2024-01-22 17:10:42 +0300

    старт

---
+++ 0_ПРАКТИКА/39_NetworkDriver/39_NetworkDriver/Program.cs
@@ -0,0 +1,12 @@
+﻿using ManagedNativeWifi;
+
+
+var arr = EnumerateNetworkSsids();
+Console.WriteLine();
+
+IEnumerable<string> EnumerateNetworkSsids()
+{
+    return NativeWifi.EnumerateAvailableNetworkSsids()
+        .Select(x => x.ToString()); // UTF-8 string representation
+}
+
