d9424dcc9f002406520137bb1572bad11d20bdc8
Igordeveloperr <g.bikmetow@gmail.com>
2024-01-23 23:10:18 +0300

    либа работает

--- 0_ПРАКТИКА/39_NetworkDriver/39_NetworkDriver/Program.cs
+++ 0_ПРАКТИКА/39_NetworkDriver/39_NetworkDriver/Program.cs
@@ -1,12 +1,21 @@
-﻿using ManagedNativeWifi;
+﻿using SimpleWifi;
 
+const string WIFI_NAME_1 = "MW42V_0866";
 
-var arr = EnumerateNetworkSsids();
-Console.WriteLine();
+Wifi net = new Wifi();
 
-IEnumerable<string> EnumerateNetworkSsids()
+// connection 
+AccessPoint accessPoint = net.GetAccessPoints().FirstOrDefault(x => x.Name == WIFI_NAME_1);
+if (accessPoint != null && !accessPoint.IsConnected)
 {
-    return NativeWifi.EnumerateAvailableNetworkSsids()
-        .Select(x => x.ToString()); // UTF-8 string representation
+    accessPoint.Connect(new AuthRequest(accessPoint));
+}
+Console.WriteLine("Done!");
+
+// disconnected
+/*if (net.ConnectionStatus == WifiStatus.Connected)
+{
+    net.Disconnect();
 }
 
+Console.WriteLine("Disconected...");*/
